85207ee80791b6db96fa735c5fd9d9cfd731079a
be989 <bassel.eizeddin@gmail.com>
2025-04-12 12:08:32 +0200

    La til spill og toppliste før merge

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TSDOblig1.Models;
+using TSDOblig1.Data; 
+using System.Linq;    // For .OrderByDescending()
 
 namespace TSDOblig1.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly BrukerContext _context; // ✅ BrukerContext for database
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, BrukerContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -23,13 +27,37 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Spill()
+    {
+        var topPlayers = _context.Brukere
+            .OrderByDescending(b => b.AntallSpill)
+            .ToList();
+
+        ViewBag.TopPlayers = topPlayers;
+
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-    public IActionResult Spill()
+    [HttpGet]
+    
+    public IActionResult HentRangering()
     {
-        return View();
+        var topPlayers = _context.Brukere
+            .OrderByDescending(b => b.AntallSpill)
+            .Select((b, i) => new {
+                Nummer = i + 1,
+                Navn = b.Navn,
+                AntallSpill = b.AntallSpill
+            })
+            .ToList();
+
+        return Json(topPlayers);
     }
+
+
 }
